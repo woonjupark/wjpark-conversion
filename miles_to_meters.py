b13cc95f710c60247220f44993131bdbacb275b6
Trevor Hillebrand <trhille@uw.edu>
2018-04-17 11:00:39 -0700

    Adds a Python function that converts miles to meters

---
+++ miles_to_meters.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 10:56:32 2018
+
+@author: trevorhillebrand
+"""
+
+#This function converts miles to meters, just like the name implies.
+
+def miles_to_meters(miles):
+    meters = 1609.34*miles
+    return meters
+
+
+    
