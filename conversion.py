fc87953644249f77cc7c36dac0a851bdf64dd5a0
Woon Ju Park <woonju.park@gmail.com>
2018-04-17 11:14:49 -0700

    Added conversion.py

---
+++ conversion.py
@@ -0,0 +1,10 @@
+# My Conversion Tools
+
+def dollars2cents(dollars):
+    cents = dollars * 100
+    return cents
+
+def gallons2liters(gallons):
+    liters = gallons * 3.78541
+    return liters
+
